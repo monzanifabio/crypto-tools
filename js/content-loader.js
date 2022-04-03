b08013735f37caf118f190f9ff4af1e8476ac3bb
FabioMonzani <renderbros@gmail.com>
2022-04-03 04:31:18 +0100

    Initial modal setup

--- js/content-loader.js
+++ js/content-loader.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   $('#navbar').load('../include/navbar.html');
+  $('#donateModalLoader').load('../include/donate-modal.html');
   var pageTitle = $('title').text();
   $('#section-title').text(pageTitle);
   getContent();
